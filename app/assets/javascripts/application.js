0e2b87951f8673879452043dad3acd5de5be1f43
Garrett Boone <Boone.Garrett@gmail.com>
2014-05-28 17:33:21 -0500

    move skrollr

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,6 +30,14 @@ $(document).ready(function() {
         $(this).animate({width:'30px'},500);
     }).trigger('mouseleave');
 
+    // Init Skrollr
+    var s = skrollr.init({
+        // render: function(data) {
+        //     //Debugging - Log the current scroll position.
+        //     console.log(data.curTop);
+        // }
+    });
+
 });
 
 
